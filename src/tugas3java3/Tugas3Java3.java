5b0dd0d060537c7136e2a1502c0333fe74a2ccee
ranggaarifwiguna <rangga164@gmail.com>
2017-07-25 15:15:25 +0700

    tugas3java3

---
+++ src/tugas3java3/Tugas3Java3.java
@@ -0,0 +1,18 @@
+package Tugas3Java3;
+
+/**
+ *
+ * @author RANGGA
+ */
+public class Tugas3Java3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame i = new NewJFrame();
+        i.setVisible(true);
+    }
+    
+}
